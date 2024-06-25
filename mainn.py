7518a812b76cafa0b2a82b382ca07e559950cac0
NarmathaRavi2003 <142740977+NarmathaRavi2003@users.noreply.github.com>
2024-06-25 13:15:52 +0530

    Update mainn.py
    Signed-off-by: NarmathaRavi2003 <142740977+NarmathaRavi2003@users.noreply.github.com>

--- mainn.py
+++ mainn.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from csv import *
-import reg
+import registerr
 import loginscreen
 import allocation
 import report
@@ -9,24 +9,24 @@ from PIL import ImageTk, Image
 
 
 class mainscr:
- 
-    def screen(self):
+    def __init__(self):
         self.mainscreen = Tk()
+    def screen(self):
         self.mainscreen.geometry('500x500')
         self.mainscreen.resizable(0, 0)
         self.mainscreen.title("MAIN")
         
-        bg_frame = Image.open(r'C:\Users\Padhmapriya\Downloads\project\project\oops.jpeg')
-        bg_photo = ImageTk.PhotoImage(bg_frame)
-        self.bg_panel = Label(self.mainscreen, image=bg_photo)
-        self.bg_panel.image = bg_photo
+        bg_frame = Image.open(r'oops.jpeg')
+        self.bg_photo = ImageTk.PhotoImage(bg_frame)
+        self.bg_panel = Label(self.mainscreen, image=self.bg_photo)
+        self.bg_panel.image = self.bg_photo
         self.bg_panel.place(width=500, height=500)
         #img1 = ImageTk.PhotoImage(Image.open("/Users/mac/Downloads/ssn laundry.jpeg"))
         #labelssn = Label(mainscreen, image = img1)
         #labelssn.pack()
         def registrationscr():
             self.mainscreen.destroy()
-            rs=reg.registerscrn()
+            rs=registerr.registerscrn()
             rs.register()
             
         def lgscr():
@@ -54,10 +54,11 @@ class mainscr:
         self.reportbutton= Button(self.mainscreen, text='REPORT', font=("gotham", 16, "bold"), width=10,height=1, bd=0,fg='red',bg='black',command=reportscreen)
         self.reportbutton.place(x=290,y=370)
 
-        self.mainscreen.mainloop()
+        #self.mainscreen.mainloop()
         '''self.orderbutton= Button(self.mainscreen, text='ORDER', font=("gotham", 16, "bold"), width=22,height=2, bd=0,fg='red',bg='black')
         self.orderbutton.place(x=120,y=300)'''
     
-
-ms=mainscr()
-ms.screen()
+if __name__ == "__main__":
+    ms = mainscr()
+    ms.screen()
+    ms.mainscreen.mainloop()
